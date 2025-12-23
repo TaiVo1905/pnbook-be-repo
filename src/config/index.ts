34dbd386803d925930b3b789bb618cafdae74faa
Duc Tai <voductaitxqt123@gmail.com>
2025-12-23 22:52:25 +0700

    chore(PNBOOK-0): update eslint, validate-branch-name configs, dependencies and remove template files

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,5 @@
-import dotenv from "dotenv";
-
 export const config = {
   port: process.env.PORT || 5001,
-  // jwtSecret: process.env.JWT_SECRET || your_secret_key,
   dbUrl: process.env.DATABASE_URL,
   aws: {
     accessKey: process.env.AWS_ACCESS_KEY,
