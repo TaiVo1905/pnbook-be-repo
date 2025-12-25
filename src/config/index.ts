24a80fafd8332743aa08755dee113cd75aa01ee1
Duc Tai <voductaitxqt123@gmail.com>
2025-12-25 11:52:05 +0700

    feat(PNBOOK-30): config google credentials

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,4 +10,12 @@ export const config = {
     secretKey: process.env.AWS_SECRET_KEY,
     bucketName: process.env.S3_BUCKET_NAME,
   },
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
 };
