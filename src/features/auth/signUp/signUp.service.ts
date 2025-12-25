680a24b25095ce24fc3ee7593f631886f35c0219
KaPhuDong <dong.ka26@student.passerellesnumeriques.org>
2025-12-25 16:20:42 +0700

    feat(PNBOOK-1): implement sign-up with email and password

--- src/features/auth/signUp/signUp.service.ts
+++ src/features/auth/signUp/signUp.service.ts
@@ -1,28 +1,20 @@
 import bcrypt from 'bcryptjs';
 import { BadRequestError } from '@/core/apiError.js'; // Sử dụng ApiError có sẵn
 import * as userRepo from '@/features/users/user.repository.js';
-import type { RegisterDto, AuthResponseDto } from './signUp.dto.js';
+import type { RegisterDto } from './signUp.dto.js';
 
-export const registerWithEmail = async (
-  data: RegisterDto
-): Promise<AuthResponseDto> => {
+export const registerWithEmail = async (data: RegisterDto): Promise<void> => {
   const existingUser = await userRepo.findUserByEmail(data.email);
   if (existingUser) {
-    throw new BadRequestError('Email already exists'); //
+    throw new BadRequestError('Email already exists');
   }
 
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(data.password, salt);
 
-  const newUser = await userRepo.createUser({
+  await userRepo.createUser({
     name: data.name,
     email: data.email,
     password: hashedPassword,
   });
-
-  return {
-    id: newUser.id,
-    name: newUser.name,
-    email: newUser.email,
-  };
 };
