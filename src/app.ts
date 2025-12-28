0722e823a2f616062ab2b9216ae905a057822f60
Duc Tai <voductaitxqt123@gmail.com>
2025-12-29 01:11:14 +0700

    ci(PNBOOK-44): add cros

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,19 @@ import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/errorHandle.middleware.js';
 import { authMiddleware } from './middlewares/auth.middleware.js';
 import { NotFoundError } from './core/apiError.js';
+import cors from 'cors';
+
+const corsOptions = {
+  origin: ['http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+};
 import { setupSwagger } from './swagger/index.swagger.js';
 
 const app = express();
+
+app.use(cors(corsOptions));
 
 app.use(helmet());
 app.use(cookieParser());
