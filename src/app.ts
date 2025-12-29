a60de0818e3ee07c8c3d20b9b1cdefab4fac00a9
Duc Tai <voductaitxqt123@gmail.com>
2025-12-29 11:29:14 +0700

    ci(PNBOOK-44): change frontend port

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { NotFoundError } from './core/apiError.js';
 import cors from 'cors';
 
 const corsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
