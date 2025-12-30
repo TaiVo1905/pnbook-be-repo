f9364b5ae5245bf05549481594daa58bea40b653
Võ Đức Tài <voductaitxqt123@gmail.com>
2025-12-30 17:41:01 +0700

    release(PNBOOK-44): deploy backend side (#22)
    * ci(PNBOOK-44): deploy api app
    
    * ci(PNBOOK-44): add cros
    
    * ci(PNBOOK-44): fix case sensitivity filename
    
    * ci(PNBOOK-44): fix cannot find module when deploying
    
    * ci(PNBOOK-44): add tsc-alias package
    
    * ci(PNBOOK-44): add db cert
    
    * ci(PNBOOK-44): update db cert
    
    * ci(PNBOOK-44): change frontend port
    
    * fix(PNBOOK-44): fix proxy
    
    * fix(PNBOOK-44): fix the build command

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,21 @@ import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/errorHandle.middleware.js';
 import { authMiddleware } from './middlewares/auth.middleware.js';
 import { NotFoundError } from './core/apiError.js';
+import cors from 'cors';
+
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+};
 import { setupSwagger } from './swagger/index.swagger.js';
 
 const app = express();
+
+app.set('trust proxy', 1);
+
+app.use(cors(corsOptions));
 
 app.use(helmet());
 app.use(cookieParser());
--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,10 +1,21 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client.js";
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client.js';
+import { config } from '../config/index.js';
+import { Pool } from 'pg';
 
 const connectionString = `${process.env.DATABASE_URL}`;
+const cert = config.digitalOcean.dbCert;
 
-const adapter = new PrismaPg({ connectionString });
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: cert,
+  },
+});
+
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
